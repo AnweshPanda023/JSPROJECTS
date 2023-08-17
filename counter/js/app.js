5b46a2cce22e78da22f8c1a67cf924c3a99a7eab
Anwesh  Panda <anweshpanda2323@gmail.com>
2023-08-17 23:55:58 +0530

    simple_counterAppadded

--- counter/js/app.js
+++ counter/js/app.js
@@ -1,25 +1,25 @@
-const endDate = "11 aug 2023 08:19 am";
-document.getElementById('end-date').innerText = "End Date : " + endDate;
-const inputs = document.querySelectorAll('input');
-const clock = () =>{
-    const end = new Date(endDate);
-    const now = new Date();
-    const diff = (end-now)/1000
-    if(diff<0){
-        inputs[0].value = 0;
-        inputs[1].value = 0;
-        inputs[2].value = 0;
-        inputs[3].value = 0;
-        return;
-    }
-    // console.log(end);
-    // console.log(now);
-    // console.log(diff);
-    inputs[0].value = Math.floor(diff/3600/24)
-    inputs[1].value = Math.floor(diff/3600)%24;
-    inputs[2].value = Math.floor(diff/60)%60;
-    inputs[3].value = Math.floor(diff)%60;
-}
-setInterval( ()=>{
-    clock();
-},1000);
+const endDate = "20 sept 2023 08:19 am";
+document.getElementById("end-date").innerText = "End Date : " + endDate;
+const inputs = document.querySelectorAll("input");
+const clock = () => {
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+  if (diff < 0) {
+    inputs[0].value = 0;
+    inputs[1].value = 0;
+    inputs[2].value = 0;
+    inputs[3].value = 0;
+    return;
+  }
+  // console.log(end);
+  // console.log(now);
+  // console.log(diff);
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
+};
+setInterval(() => {
+  clock();
+}, 1000);
--- note_taking_app/js/app.js
+++ note_taking_app/js/app.js
@@ -1,19 +1,3 @@
-const addbtn = document.querySelector("#addBtn");
-const main = document.querySelector("#main");
-const saveNotes = () => {
-  const notes = document.querySelectorAll(".note textarea");
-  //   console.log(notes);
-  const data = [];
-  notes.forEach((note) => {
-    data.push(note.value);
-  });
-  if (data.length === 0) {
-    localStorage.removeItem("notes");
-  } else {
-    localStorage.setItem("notes", JSON.stringify(data));
-  }
-  //   console.log(data);
-};
 {
   /* <div class="note">
         <div class="tool">
@@ -24,41 +8,23 @@ const saveNotes = () => {
       </div> */
 }
 
-addbtn.addEventListener("click", () => {
-  addNote();
-});
+const btn = document.querySelector("#addBtn");
+const main = document.querySelector("#main");
 
-const addNote = (text = "") => {
+const addNotes = () => {
   const note = document.createElement("div");
   note.classList.add("note");
-  note.innerHTML = `
-        <div class="tool">
+  note.innerHTML = `<div class="tool">
           <i class="save fa-solid fa-floppy-disk fa-2x __save"></i>
-          <i class="trash fa-solid  fa-trash fa-2x"></i>
+          <i class="trash fa-solid fa-trash fa-2x"></i>
         </div>
-        <textarea>${text}</textarea>
-      `;
-
-  note.querySelector(".save").addEventListener("click", () => {
-    saveNotes();
-  });
+        <textarea></textarea>`;
   note.querySelector(".trash").addEventListener("click", () => {
     note.remove();
-    saveNotes();
-  });
-  note.querySelector("textarea").addEventListener("focusout", function () {
-    saveNotes();
   });
   main.appendChild(note);
-  saveNotes();
 };
-(function () {
-  const lsNotes = JSON.parse(localStorage.getItem("notes"));
-  if (lsNotes === null) {
-    addNote();
-  } else {
-    lsNotes.forEach((lsNote) => {
-      addNote(lsNote);
-    });
-  }
-})();
+
+btn.addEventListener("click", () => {
+  addNotes();
+});
